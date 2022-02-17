76e135e5c804850bccc0c348d3b478204f92c8c1
Marc Medhat <marcmedhat6211@gmail.com>
2022-02-18 00:07:42 +0200

    adjusting the js in the address section in address and payment page

--- js/pages/address-and-payment.js
+++ js/pages/address-and-payment.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-
+    var addressForm = $("#address_form");
+    var shippingDetailsContainer = $("#shipping_details_container");
+    addressForm.validate();
+    let addressFormInputs = addressForm.find("input");
+    addressFormInputs.each(function() {
+        let inputName = $(this).attr("name");
+        console.log(inputName);
+        $(this).on("input", function() {
+            shippingDetailsContainer.find(`.${inputName}`).text($(this).val());
+        });
+    });
 });
