0b3a9342a92d9a3f773d051e7f4c0ce138fbe5d3
Marc Medhat <marcmedhat6211@gmail.com>
2022-02-18 16:21:31 +0200

    adjusting payment and address details page

--- js/pages/address-and-payment.js
+++ js/pages/address-and-payment.js
@@ -1,13 +1,33 @@
 $(document).ready(function() {
+    // address info
     var addressForm = $("#address_form");
     var shippingDetailsContainer = $("#shipping_details_container");
-    addressForm.validate();
     let addressFormInputs = addressForm.find("input");
     addressFormInputs.each(function() {
         let inputName = $(this).attr("name");
-        console.log(inputName);
+        let detailContainer = shippingDetailsContainer.find(`.${inputName}`);
+        let oldValue = detailContainer.text();
         $(this).on("input", function() {
-            shippingDetailsContainer.find(`.${inputName}`).text($(this).val());
+            detailContainer.text($(this).val());
+            if (detailContainer.text() == "") {
+                detailContainer.text(oldValue);
+            }
         });
     });
+
+    // coupon input
+    let couponCodeForm = $("#coupon_code_form");
+    let couponInput = couponCodeForm.find("#coupon_input");
+    let removeCouponBtn = couponCodeForm.find("#remove_coupon_btn");
+    couponInput.on("input", function() {
+        if ($(this).val() == "") {
+            removeCouponBtn.removeClass("show");
+        } else {
+            removeCouponBtn.addClass("show");
+        }
+    });
+    removeCouponBtn.on("click", function() {
+        couponInput.val("");
+        $(this).removeClass("show");
+    })
 });
