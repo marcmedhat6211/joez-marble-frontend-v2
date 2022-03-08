d7f56e2f1faa6e5394784471e14b7cb2165f586e
Marc Medhat <marcmedhat6211@gmail.com>
2022-03-08 23:09:41 +0200

    home page slider scrollbar animation and user feedback popup

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,70 +1,73 @@
 var homePageContainer = $("#home_page_container");
-$(document).ready(function() {
-    // banner swiper
-    var bannerSwiper = new Swiper('.swiper-container.banner-swiper', {
-        loop: false,
+$(document).ready(function () {
+  // banner swiper
+  var bannerSwiper = new Swiper(".swiper-container.banner-swiper", {
+    loop: false,
+    slidesPerView: 1,
+    centeredSlides: true,
+    watchSlidesProgress: true,
+    effect: "coverflow",
+    coverflowEffect: {
+      rotate: 30,
+      slideShadows: false,
+    },
+    lazy: {
+      preloadImages: false,
+      loadPrevNext: true,
+      loadPrevNextAmount: 2,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 2000,
+    },
+    breakpoints: {
+      991.98: {
         slidesPerView: 1,
-        centeredSlides: true,
-        watchSlidesProgress: true,
-        effect: 'coverflow',
-        coverflowEffect: {
-            rotate: 30,
-            slideShadows: false,
-        },
-        lazy: {
-            preloadImages: false,
-            loadPrevNext: true,
-            loadPrevNextAmount: 2,
-        },
-        scrollbar: {
-            el: '.swiper-scrollbar',
-            draggable: true,
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'bullets',
-            clickable: true,
-        },
-        // autoplay: {
-        //     delay: 2000,
-        // },
-        breakpoints: {
-            991.98: {
-                slidesPerView: 1,
-                spaceBetween: 0,
-            },
-        },
-    });
+        spaceBetween: 0,
+      },
+    },
+  });
 
-    // let scrollBarDrag = homePageContainer.find(".banner-section .swiper-scrollbar .swiper-scrollbar-drag");
-    // let scrollBarOldInitialWidth = scrollBarDrag.outerWidth();
-    // bannerSwiper.on("slideChange", function() {
-    //     let currentSlideNumber = bannerSwiper.activeIndex + 1;
-    //     scrollBarDrag.css("width", scrollBarOldInitialWidth * currentSlideNumber);
-    // });
+  const bannerSlider = $(".swiper-container.banner-swiper");
+  const scrollBar = bannerSlider.find(".scrollbar");
+  const totalSlidesNumber = bannerSlider.find(".swiper-slide").length;
+  swiperScrollBarHandler(scrollBar, 1, totalSlidesNumber);
 
+  console.log((1 / 3) * 100);
+  bannerSwiper.on("slideChange", function () {
+    const currentSlideNumber = bannerSwiper.activeIndex + 1;
+    swiperScrollBarHandler(scrollBar, currentSlideNumber, totalSlidesNumber);
+  });
 
-
-    // reviews swiper
-    var reviewsSwiper = new Swiper('.swiper-container.reviews-swiper', {
-        loop: false,
-        slidesPerView: 1,
+  // reviews swiper
+  var reviewsSwiper = new Swiper(".swiper-container.reviews-swiper", {
+    loop: false,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    lazy: {
+      preloadImages: false,
+      loadPrevNext: true,
+      loadPrevNextAmount: 4,
+    },
+    navigation: {
+      nextEl: ".swiper-btn-next-reviews",
+      prevEl: ".swiper-btn-prev-reviews",
+    },
+    breakpoints: {
+      991.98: {
+        slidesPerView: 2,
         spaceBetween: 20,
-        lazy: {
-            preloadImages: false,
-            loadPrevNext: true,
-            loadPrevNextAmount: 4,
-        },
-        navigation: {
-            nextEl: '.swiper-btn-next-reviews',
-            prevEl: '.swiper-btn-prev-reviews'
-        },
-        breakpoints: {
-            991.98: {
-                slidesPerView: 2,
-                spaceBetween: 20,
-                watchSlidesVisibility: true,
-            },
-        }
-    });
+        watchSlidesVisibility: true,
+      },
+    },
+  });
 });
+
+const swiperScrollBarHandler = (scrollBar, currentSlideNumber, slidesCount) => {
+  const scrollBarNewWidth = `${(currentSlideNumber / slidesCount) * 100}%`;
+  scrollBar.css({ width: scrollBarNewWidth });
+};
