b984b2aca167e38dc0d1aeb1a942764fec543752
marcmedhat6211 <marcmedhat6211@gmail.com>
2023-03-21 00:23:08 +0200

    adjusting the customizable gifts page

--- js/pages/customizable-gifts.js
+++ js/pages/customizable-gifts.js
@@ -15,6 +15,11 @@ const textBlock = $("#text_block");
 const textInput = textBlock.find("input[name=shape-text]");
 const charsLimitText = textBlock.find("#shape_chars_limit");
 
+// shape figure path
+const shapeFigurePath = "images/gifts/shapes";
+
+let canAddToShape = true;
+
 $(document).ready(function () {
   // set chars limit
   setCharsLimitHandler();
@@ -24,9 +29,73 @@ $(document).ready(function () {
     length = CHARS_LIMIT - length;
     charsLimitText.text(length);
   });
+
+  //================================================ WHEN CHANGING MAIN SHAPES ==============================================
+  $("body").on("click", ".page-block .shapes-block .shape", function () {
+    const svgName = $(this).data("svg-name");
+    $("#shape #shape_figure").remove();
+    drawMainShape(svgName, $("#shape"));
+    convertSvgToIcon($("i#shape_figure"));
+    $(".form-check.shape").removeClass("checked");
+    $(this).addClass("checked");
+  });
+  //================================================ END WHEN CHANGING MAIN SHAPES ==============================================
+
+  //================================================ WHEN WRITING TEXT ON SHAPE ==============================================
+  $("body").on("input", ".page-block .text-block #shape_text", function () {
+    $("#shape .shape-text.shape__content").text($(this).val());
+  });
+  //================================================ END WHEN WRITING TEXT ON SHAPE ==============================================
+
+  //================================================ COLOR PICKER ==============================================
+  $("#color_picker").spectrum();
+  $("#color_picker").on("move.spectrum", function (e, tinycolor) {
+    const HexColor = tinycolor.toHexString();
+    $("#shape .shape-text.shape__content").css({
+      color: HexColor,
+    });
+  });
+  //================================================ END COLOR PICKER ==============================================
+
+  //================================================ CHANGE THE POSITION OF THE SHAPE CONTENT USING ARROWS ==============================================
+  $("body").on("mousedown", "#shape .page-btn.move-btn", function () {
+    const shapeContent = $("#shape .shape__content");
+    if ($(this).hasClass("up")) {
+      shapeContent.css({
+        top: `${convertCssPropertyInPxToInt(shapeContent.css("top")) - 5}px`,
+      });
+    } else if ($(this).hasClass("right")) {
+      shapeContent.css({
+        left: `${convertCssPropertyInPxToInt(shapeContent.css("left")) + 5}px`,
+      });
+    } else if ($(this).hasClass("down")) {
+      shapeContent.css({
+        top: `${convertCssPropertyInPxToInt(shapeContent.css("top")) + 5}px`,
+      });
+    } else if ($(this).hasClass("left")) {
+      shapeContent.css({
+        left: `${convertCssPropertyInPxToInt(shapeContent.css("left")) - 5}px`,
+      });
+    }
+  });
+  //================================================ END CHANGE THE POSITION OF THE SHAPE CONTENT USING ARROWS ==============================================
 });
 
 const setCharsLimitHandler = () => {
   textInput.attr("maxlength", CHARS_LIMIT);
   charsLimitText.text(CHARS_LIMIT);
+};
+
+const drawMainShape = (svgName, containerElement) => {
+  const mainShape = $("<i>").attr({
+    id: "shape_figure",
+    class: "convert-svg",
+    "data-src": `../../${shapeFigurePath}/${svgName}`,
+  });
+
+  mainShape.appendTo(containerElement);
+};
+
+const convertCssPropertyInPxToInt = (cssProperty) => {
+  return parseInt(cssProperty.replace("px", ""));
 };
