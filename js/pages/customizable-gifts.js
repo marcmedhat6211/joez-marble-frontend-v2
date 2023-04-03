ce3f8d336a6ede4dab993f923f7e854dc3473938
marcmedhat6211 <marcmedhat6211@gmail.com>
2023-04-03 22:26:57 +0200

    adjusting some logic for the customizable gifts page

--- js/pages/customizable-gifts.js
+++ js/pages/customizable-gifts.js
@@ -81,24 +81,87 @@ $(document).ready(function () {
   //================================================ END CHANGE THE POSITION OF THE SHAPE CONTENT USING ARROWS ==============================================
 
   //================================================ ADD ICONS TO SHAPE ==============================================
-  $("body").on("click", "#icons_block .icon", function () {
-    const icon = $(this);
-    const path = icon.attr("id");
-    $("#shape .shape__content.shape-icon").empty();
-    drawIcon(path, $("#shape .shape__content.shape-icon"));
-    convertSvgToIcon($("#shape .shape__content.shape-icon i"));
-  });
+  $("body").on(
+    "click",
+    "#icons_block .icon, #numbers_block .icon",
+    function () {
+      const icon = $(this);
+      const path = icon.attr("id");
+      const clicked = $(this);
+      console.log(clicked);
+      $("#shape .shape__content.shape-icon").empty();
+      drawIcon(
+        path,
+        $("#shape .shape__content.shape-icon"),
+        clicked.hasClass("icon-type") ? "icon" : "number"
+      );
+      convertSvgToIcon($("#shape .shape__content.shape-icon i"));
+    }
+  );
   //================================================ END ADD ICONS TO SHAPE ==============================================
 
   //================================================ FILL ICONS IN SHAPE ==============================================
   $("body").on("change", "#fill_shape", function () {
     if ($(this).is(":checked")) {
-      $("#shape .shape__content.shape-icon").addClass("filled");
+      $("#shape .shape__content.shape-icon")
+        .removeClass("number-type")
+        .addClass("icon-filled");
     } else {
-      $("#shape .shape__content.shape-icon").removeClass("filled");
+      $("#shape .shape__content.shape-icon").removeClass("icon-filled");
+    }
+  });
+
+  $("body").on("change", "#fill_shape_numbers", function () {
+    if ($(this).is(":checked")) {
+      $("#shape .shape__content.shape-icon")
+        .removeClass("icon-type")
+        .addClass("number-filled");
+    } else {
+      $("#shape .shape__content.shape-icon").removeClass("number-filled");
     }
   });
   //================================================ END FILL ICONS IN SHAPE ==============================================
+
+  //================================================ MARBLES SLIDER ==============================================
+  const marblesSwiper = new Swiper(".swiper-container#marbles_slider", {
+    loop: true,
+    slidesPerView: 5,
+    spaceBetween: 20,
+    lazy: {
+      preloadImages: false,
+      loadPrevNext: true,
+      loadPrevNextAmount: 4,
+    },
+    navigation: {
+      nextEl: ".swiper-btn-next-marbles",
+      prevEl: ".swiper-btn-prev-marbles",
+    },
+    // breakpoints: {
+    //   991.98: {
+    //     slidesPerView: 2,
+    //     spaceBetween: 20,
+    //     watchSlidesVisibility: true,
+    //   },
+    // },
+  });
+  //================================================ END MARBLES SLIDER ==============================================
+
+  //================================================ HANDLE MARBLES CLICKS ==============================================
+  $("body").on("click", "#marbles_slider .marble-container", function () {
+    $("#marbles_slider .marble-container").not($(this)).removeClass("active");
+    $(this).addClass("active");
+    const imgPath = $(this).data("src");
+    const shapeFigureContainer = $(".shape-figure__container");
+    shapeFigureContainer.find(".shape__content.shape__marble-image").remove();
+    drawMarbleBg(imgPath, shapeFigureContainer);
+  });
+  //================================================ END HANDLE MARBLES CLICKS ==============================================
+
+  //================================================ HANDLE THE PICTURE SCREENSHOT AND SEND IT TO BACKEND ==============================================
+  $("body").on("click", "#submit_gift_btn", function () {
+    takeshot();
+  });
+  //================================================ END HANDLE THE PICTURE SCREENSHOT AND SEND IT TO BACKEND ==============================================
 });
 
 const setCharsLimitHandler = () => {
@@ -120,11 +183,48 @@ const convertCssPropertyInPxToInt = (cssProperty) => {
   return parseInt(cssProperty.replace("px", ""));
 };
 
-const drawIcon = (iconPath, destinationContainer) => {
+const drawIcon = (iconPath, destinationContainer, iconType) => {
   const icon = $("<i>").attr({
-    class: "convert-svg",
+    class: `convert-svg ${iconType === "icon" ? "icon-type" : "number-type"}`,
     "data-src": iconPath,
   });
 
   icon.appendTo(destinationContainer);
+};
+
+const drawMarbleBg = (imagePath, destinationContainer) => {
+  const image = $("<img>").attr({
+    class: "shape__content shape__marble-image",
+    src: imagePath,
+    alt: getImageNameFromPath(imagePath),
+  });
+
+  image.appendTo(destinationContainer);
+};
+
+const getImageNameFromPath = (imagePath) => {
+  const imagePathArr = imagePath.split("/");
+  const nameForPath = imagePathArr[imagePathArr.length - 1];
+  const nameForPathArr = nameForPath.split("-");
+  let adjustedNameForPathArr = [];
+  for (const namePart of nameForPathArr) {
+    adjustedNameForPathArr.push(
+      namePart.charAt(0).toUpperCase() + namePart.slice(1)
+    );
+  }
+
+  return adjustedNameForPathArr.join(" ");
+};
+
+// Define the function
+// to screenshot the div
+const takeshot = () => {
+  let div = $("#shape").get(0);
+  // Use the html2canvas
+  // function to take a screenshot
+  // and append it
+  // to the output div
+  html2canvas(div).then((canvas) => {
+    document.getElementById("output").appendChild(canvas);
+  });
 };
